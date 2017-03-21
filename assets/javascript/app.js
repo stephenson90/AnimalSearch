5cc214a3f96cfe2a2b852e73575c96af2da4eabf
Jean Davilus <jsd6@njit.edu>
2017-03-21 16:01:59 -0400

    everything works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 var giphyLink ="http://api.giphy.com/v1/gifs/search?q=";
-var keyPass = "&api_key=dc6zaTOxFJmzC&limit=5";
+var keyPass = "&api_key=dc6zaTOxFJmzC&limit=6";
 
 var animals = [ "chicken", "cow", "goat", "pig","horse", "donkey"];
 
+
+//displaying the buttons on the page
 function addButtons(){
 
 	$("#button").empty();
@@ -20,7 +22,7 @@ function addButtons(){
 
   addButtons();
 
-
+// adding user input to the array and then display all the buttons
 $("#addedAni").on("click", function(event){
 	event.preventDefault();
 	var animal = $("#animals").val().trim();
@@ -41,24 +43,45 @@ function animalInfo() {
           url: queryURL,
           method: "GET"
         }).done(function(response) {
-        	var aniData = response.data
-
+        	var aniData = response.data;
+          var picRate="";
+          
         	for (var i = 0; i<aniData.length; i++){
-        	var urlPhoto = aniData[i].images.fixed_height.url;
-        	var unePlace = $("<div>");
-        	//var rating = information.
-        	var photo = $("<img>").attr("src", urlPhoto); 
-        	console.log(urlPhoto);       	
-        	unePlace.append(photo);
-        	$(".displayThings").prepend(unePlace);
-
-          //$(".displayThings").html(JSON.stringify(response));
+        		var urlPhoto = aniData[i].images.fixed_height.url;
+            var urlStill = aniData[i].images.fixed_height_still.url;
+            var urlAnimate = urlPhoto;
+        		picRate = aniData[i].rating;
+        		var unePlace = $("<div>");           
+        		var paragr = $("<p class='forma'>");
+        		//$(".forma").css({"font-size":"16px", "color":"white ","padding":"5px" });            
+        		var photo = $("<img class='leftLea'>").attr({"src": urlPhoto, "data-still":urlStill, "data-animate":urlAnimate, "data-state":"still"}); 
+        		paragr.append("Rating: "+picRate);
+        		unePlace.append(paragr);
+        		console.log(urlPhoto);       	
+        		unePlace.append(photo);
+        		//$(".leftLea").css("padding","5px");
+        		$(".displayThings").prepend(unePlace);
+          
           addButtons();
           }
-     
-        });
+     $(".forma").css({"font-size":"16px", "color":"white ","padding":"5px" });
+     $(".leftLea").css({"padding":"5px"}); 
 
-    	
+        $(".leftLea").on("click", function() {
+      var state = $(this).attr("data-state");    
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+
+
+        });    	
       }
 
       $(document).on("click", ".anim", animalInfo);
+
+   
